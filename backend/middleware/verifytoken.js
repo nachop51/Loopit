b5c3ed0a43409202f10cdb75d2b925027f61a11f
valentin repetto <valentinrepetto12345@gmail.com>
2022-10-01 15:23:34 -0300

    add auth in controllers,too code formatter

--- backend/middleware/verifytoken.js
+++ backend/middleware/verifytoken.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken')
-const key = require('../config').key
+const jwt = require("jsonwebtoken");
+const key = require("../config").key;
 
-const verifytoken = (req,res,next) =>{
-    if (req.url === '/login' || req.url === '/register'){
-        next()
-    }else{
-        const token = req.header('auth-token')
-    if (!token){
-        return res.status(401).json({error: "acceso denegado"})
+const verifytoken = (req, res, next) => {
+  if (req.url === "/login" || req.url === "/register") {
+    next();
+  } else {
+    const token = req.header("auth-token");
+    if (!token) {
+      return res.status(401).json({ error: "Access denied" });
     }
     try {
-        const verificar = jwt.verify(token, key)
-        req.user = verificar
-        next()
+      const verificar = jwt.verify(token, key);
+      req.user = verificar;
+      next();
     } catch (error) {
-        return res.status(401).json({error: "acceso denegado, token no es valido"})
+      return res.status(401).json({ error: "Access denied, invalid token" });
     }
-    }
-}
+  }
+};
 
 module.exports = verifytoken;
