6c831acfec070f2de626c29e0932a66d388595ff
santiago <santiagoneira52@gmail.com>
2022-10-02 04:00:40 -0300

    try to implement test with jest

--- frontend/src/__tests__/Logo.test.js
+++ frontend/src/__tests__/Logo.test.js
@@ -1,10 +1,11 @@
+import React from "react";
 // import renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
-import { App } from "../components/App";
+import Footer from "../components/Footer/index.jsx";
 
 describe("Logo renders correctly", () => {
-  it("Debe existir un componente img en el dom", () => {
-    const { container } = render(<App />);
+  it("Debe existir un componente Footer en el dom", () => {
+    const { container } = render(<Footer />);
     expect(container).toMatchSnapshot();
 
     // The following line is not needed, but it is useful to see the output
