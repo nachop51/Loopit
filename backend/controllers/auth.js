4be3aae9afb19fd7f646b89b607f0415a05f0aa6
MateoArbini <mateoarbini1998@gmail.com>
2022-10-03 22:29:18 +0000

    Added some comments to the files

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,21 +4,26 @@ const bcrypt = require("bcrypt");
 const key = require("../config").key;
 const jwt = require("jsonwebtoken");
 
+// Authentication method for the login
 const login = async (req, res) => {
   let { body } = req;
   let { username, password, email } = body;
+  // If the user doesnt pass the required data, status code 400 is launched
   if (!password || (!username && !email)) {
     return res.status(400).json({
       state: "error",
-      error: "Missing data",
+      error: "Bad Request - Missing data",
     });
   }
   let query = "";
+  // Here we corroborate if the user passed the email or the username
   if (email !== undefined) {
+    // Query format with the email
     query = mpq.queryBuilder("SELECT * FROM users WHERE email = :email;", {
       email: email,
     });
   } else {
+    // Query format with the username
     query = mpq.queryBuilder(
       "SELECT * FROM users WHERE username = :username;",
       {
@@ -27,24 +32,31 @@ const login = async (req, res) => {
     );
   }
   console.log(query);
+  // Query
   conexion.query(query, async (error, results) => {
+    // If there is an error with the query
     if (error)
       res.json({
         state: "Error",
         error: error,
       });
+      // If the query did not bring data from the database, but there was not an error
     else if (results.length == 0) {
       res.status(400).json({
-        state: "This user is not registered",
+        state: "Bad request - This user is not registered",
       });
     } else {
+      // If the query brought data from the database and there was not an error
       try {
         let passBD = results[0].password;
+        // Here we compare with the bcrypt library, the two passwords, the encrypted and the other one.
         const compare = await bcrypt.compare(password, passBD);
+        // If they dont match
         if (!compare) {
           res.status(401).json({
-            state: "Password or user incorrect",
+            state: "Authentication Error - Password or user incorrect",
           });
+        // If they match
         } else {
           const token = jwt.sign({ username: results[0].username }, key, {
             expiresIn: "1h",
@@ -63,15 +75,18 @@ const login = async (req, res) => {
   });
 };
 
+// Authentication method for the register
 const regis = async (req, res) => {
   const { body } = req;
   const { username, fullname, password, email } = body;
+  // If the user did not pass the required information, status code 400 is launched 
   if (!username || !fullname || !password || !email) {
     res.status(400).json({
       state: "Error",
-      error: "Missing data",
+      error: "Bad Request - Missing data",
     });
   }
+  // If the user pass all information required, the query format is the next one
   let query = mpq.queryBuilder(
     "SELECT * FROM users WHERE username = :username OR email = :email;",
     {
@@ -79,30 +94,37 @@ const regis = async (req, res) => {
       email: email,
     }
   );
+  // Query
   conexion.query(query, async (error, results) => {
+    // If there is an error with the query
     if (error) {
       res.json({
         state: "Error",
         error: error,
       });
+      // If there is data in the Database
     } else if (results.length != 0) {
       if (results[0].email === email) {
         res.status(400).json({
-          state: "This email already exists",
+          state: "Bad Request - This email already exists",
         });
+      // If the username already exists
       } else {
         res.status(400).json({
-          state: "This username already exists",
+          state: "Bad Request - This username already exists",
         });
       }
+      // If there is no data in the database
     } else if (results.length == 0) {
       try {
+        // Here we encrypt the password passed by the user
         const passcrypt = await bcrypt.hash(password, 8);
         const create_at = new Date()
           .toISOString()
           .slice(0, 19)
           .replace(/-/g, "/")
           .replace("T", " ");
+        // Query to register the new user
         conexion.query(
           "INSERT INTO users SET ?",
           {
@@ -114,11 +136,13 @@ const regis = async (req, res) => {
             update_at: create_at,
           },
           (error, results) => {
+            // If there was an error with the query
             if (error) {
               res.json({
                 state: "Error",
                 error: error,
               });
+            // If the query was correct
             } else {
               res.status(200).json({
                 state: "Registered",
@@ -128,18 +152,20 @@ const regis = async (req, res) => {
           }
         );
       } catch {
+        // Bcrypt is async, so if MUST be inside a try-catch
         res.status(400).json({
-          state: "error",
+          state: "Error",
         });
       }
     } else {
-      res.status(401).json({
-        state: "This user or email already exists",
+      res.status(400).json({
+        state: "Bad Request - This user or email already exists",
       });
     }
   });
 };
 
+// Authentication method for the password - Not finished yet
 const updatePass = (req, res) => {
   const user = req.body.user;
   const pass = req.body.pass;
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,35 +2,44 @@ const conexion = require("../database/db");
 const mpq = require("mysql-query-placeholders");
 
 const getUser = (req, res) => {
+  // This conditional works in the case that a user wants the data of a specific user.
   if (req.params.username) {
     let { username } = req.params;
     let query = "SELECT * FROM users WHERE ?";
+    // Query
     conexion.query(query, { username: username }, (errors, results) => {
       if (errors) {
-        res.status(404).json({
-          status: "Error",
+        // Here, we cover the case of an Internal Server Error (500)
+        res.status(500).json({
+          status: "Error - Internal Server Error",
           error: errors,
         });
+      // In this case, the result brought data from the database - Status 200
       } else if (results.length !== 0) {
         res.status(200).json({
           status: "OK",
           data: results[0],
         });
+        // Here, we cover the case that the data doesnt match in the Database (404)
       } else {
-        res.status(200).json({
-          status: "Error",
+        res.status(404).json({
+          status: "Error - Not found",
           error: "Username not exists",
         });
       }
     });
+  // From here, we work the case that users wants all users data.
   } else {
     query = "SELECT * FROM users";
+    // Query
     conexion.query(query, (errors, results) => {
+      // Here, we cover the case that there is an Internal Server Error
       if (errors) {
-        res.status(404).json({
-          status: "Error",
+        res.status(500).json({
+          status: "Error - Internal Server Error",
           error: errors,
         });
+        // Here, the query brought data from the database, code 200
       } else {
         res.status(200).json({
           status: "OK",
@@ -40,7 +49,7 @@ const getUser = (req, res) => {
     });
   }
 };
-
+// Here we export the module, in order to use it in routes/routeUser
 module.exports = {
   getUser: getUser,
 };
