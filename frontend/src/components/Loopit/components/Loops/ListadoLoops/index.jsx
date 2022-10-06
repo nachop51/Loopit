7a431e6aac81c4e811c24206114acc83e0551e60
santiago <santiagoneira52@gmail.com>
2022-10-06 20:11:10 -0300

    Loops component estructure

---
+++ frontend/src/components/Loopit/components/Loops/ListadoLoops/index.jsx
@@ -0,0 +1,14 @@
+import ItemLoop from "../ItemLoop";
+
+const ListadoLoops = () => {
+  return (
+    <div>
+      <ItemLoop />
+      <ItemLoop />
+      <ItemLoop />
+      <ItemLoop />
+    </div>
+  );
+};
+
+export default ListadoLoops;
