67247e5874f45c92a3beb214f0e0c0912d6e74a3
santiago <santiagoneira52@gmail.com>
2022-10-12 16:29:13 -0300

    styling aside items

--- frontend/src/components/Loopit/Aside/index.jsx
+++ frontend/src/components/Loopit/Aside/index.jsx
@@ -4,12 +4,15 @@ const Aside = () => {
   return (
     <aside className="aside">
       <div className="aside-container">
-        <h2 className="aside-title">About Loopit</h2>
-        <p className="aside-text">
-          Loopit is a social media platform for programmers to share their code
-          with the world. Create a profile, upload your code, and share it with
-          your friends, followers and the world.
-        </p>
+        <div className="item-container">
+          <h2 className="aside-title">About Loopit</h2>
+          <p className="aside-text">
+            Loopit is a social media platform for programmers to share their
+            code with the world. Create a profile, upload your code, and share
+            it with your friends, followers and the world.
+          </p>
+          <span className="aside-view-yarn">Seguir hilo</span>
+        </div>
       </div>
     </aside>
   );
