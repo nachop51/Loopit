b90eb36fd7574755c9af2ab9bef473a0ea08c13e
Nacho Peralta <nachoperalta0@gmail.com>
2022-10-12 12:44:58 -0300

    strcture optimiaztion

---
+++ frontend/src/components/Loopit/Aside/index.jsx
@@ -0,0 +1,18 @@
+import "./Aside.css";
+
+const Aside = () => {
+  return (
+    <aside className="aside">
+      <div className="aside-container">
+        <h2 className="aside-title">About Loopit</h2>
+        <p className="aside-text">
+          Loopit is a social media platform for programmers to share their code
+          with the world. Create a profile, upload your code, and share it with
+          your friends, followers and the world.
+        </p>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
---
+++ frontend/src/components/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+const Favorites = () => {
+  return (
+    <>
+      <h1>Lista de loops favoritos</h1>
+    </>
+  );
+};
+
+export default Favorites;
