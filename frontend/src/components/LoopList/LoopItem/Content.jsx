9cf5826ff5ce167d0b73a85d8fc8843fdd5e9fd9
Nachop51 <nachoperalta0@gmail.com>
2022-10-16 22:22:00 -0300

    SyntaxHighlighter added

--- frontend/src/components/LoopList/LoopItem/Content.jsx
+++ frontend/src/components/LoopList/LoopItem/Content.jsx
@@ -1,13 +1,19 @@
 import "./Content.css";
 import PropTypes from "prop-types";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Content = ({ content, description, language }) => {
   return (
     <div className="loop-content">
       {description}
-      <pre>
-        <code className="code">{content}</code>
-      </pre>
+      <SyntaxHighlighter
+        language={language}
+        style={atomOneDark}
+        showLineNumbers
+      >
+        {content}
+      </SyntaxHighlighter>
     </div>
   );
 };
