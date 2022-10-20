99bb2a50e515fc379b930ad4e93e29b4e5d666e5
valentin repetto <valentinrepetto12345@gmail.com>
2022-10-19 22:22:43 -0300

    fix delete route

--- backend/routes/routeFollowers.js
+++ backend/routes/routeFollowers.js
@@ -2,6 +2,6 @@ const routeFollower = require("express").Router();
 const { addFollower, deleteFollower } = require("../controllers/followers");
 
 routeFollower.post("/add/:follow_id", addFollower);
-routeFollower.delete("/delete/:username", deleteFollower);
+routeFollower.post("/delete/:username", deleteFollower);
 
 module.exports = routeFollower;
