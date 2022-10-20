fccded6d00065b74fafa168622b0850a6b625b80
santiago <santiagoneira52@gmail.com>
2022-10-20 09:16:03 -0300

    changes in comment page

--- frontend/src/components/pages/Comments/index.jsx
+++ frontend/src/components/pages/Comments/index.jsx
@@ -2,6 +2,7 @@ import "./Comments.css";
 import loopit from "../../../api/loopit";
 import LoopItem from "../../LoopList/LoopItem";
 import LoadEditor from "../../Editor";
+import User from "../../LoopList/LoopItem/User";
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -26,13 +27,50 @@ const Comments = () => {
         <div className="comments">
           <h1 className="heading-primary">Comments</h1>
           <LoopItem loop={loop} collection={"created"} />
-          <LoadEditor
-            width={"100%"}
-            height={"350px"}
-            language={loop.language.name}
-            setCode={setCode}
-          />
-          <div className="comments-container">
+
+          <div className="comments-list">
+            <div className="comments-container">
+              <div className="commment-author">
+                <User username={"Nachop hardcode"} />
+              </div>
+              <div className="comment-list_comment">
+                <p>
+                  Muy interesantes su aporte en verdad, justo estaba necesitando
+                  hacer eso
+                </p>
+              </div>
+            </div>
+            <div className="comments-container">
+              <div className="commment-author">
+                <User username={"Nachop hardcode"} />
+              </div>
+              <div className="comment-list_comment">
+                <p>091834859 hablenme nenas</p>
+              </div>
+            </div>
+            <div className="comments-container">
+              <div className="commment-author">
+                <User username={"Nachop hardcode"} />
+              </div>
+              <div className="comment-list_comment">
+                <p>nada que ver jajajs</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="add-comment-container">
+            <h2 className="heading-secondary">Add a comment</h2>
+            <LoadEditor
+              width={"80%"}
+              height={"300px"}
+              language={loop.language.name}
+              setCode={setCode}
+            />
+            {/* description */}
+            <button className="add-comment-btn btn btn--primary">Create</button>
+          </div>
+
+          {/* <div className="comments-container">
             <div className="comments-list">
               {loop.comments.map((comment) => {
                 return (
@@ -42,7 +80,7 @@ const Comments = () => {
                 );
               })}
             </div>
-          </div>
+          </div> */}
         </div>
       ) : (
         <div>Loading...</div>
