20b8ad95eafe859bfaf04ded7b4028c7be1ba51c
valentin repetto <valentinrepetto12345@gmail.com>
2022-10-21 13:45:52 -0300

    fix security

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,22 +4,27 @@ const { Sequelize } = require("sequelize");
 //config connection of mysql database with mysql2
 const conexion = mysql.createPool({
   connectionLimit: 10,
-  host: "54.94.125.72",
-  user: "loopit",
-  password: "loop",
-  database: "loopit",
+  host: process.env.IP_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASS_DB,
+  database: process.env.NAME_DB,
 });
 
 //config sequelize for use mysql database
-const sequelize = new Sequelize("loopit", "loopit", "loop", {
-  host: "54.94.125.72",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const sequelize = new Sequelize(
+  process.env.NAME_DB,
+  process.env.USER_DB,
+  process.env.PASS_DB,
+  {
+    host: process.env.IP_DB,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = { conexion: conexion, sequelize: sequelize };
