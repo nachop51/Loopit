d5a0d7f57fb594beeb21dcad242879f4f4527f50
santiago <santiagoneira52@gmail.com>
2022-10-21 02:30:54 -0300

    aside items in dark mode

--- frontend/src/components/Loopit/Aside/AsideItem.jsx
+++ frontend/src/components/Loopit/Aside/AsideItem.jsx
@@ -1,14 +1,33 @@
+import { BsChat } from "react-icons/bs";
+import { IoBookmark } from "react-icons/io5";
+import { MdRecommend } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const AsideItem = ({ populates }) => {
+  // const handleClick = () => {
+
+  // }
+
   return (
     <>
       <div className="item-container">
-        <h2 className="aside-title">{populates.title}</h2>
-        <p className="aside-text">{populates.content}</p>
-        {/* <Link to={}> */}
-        <p className="aside-view-yarn">Follow thread</p>
-        {/* </Link> */}
+        <h2 className="aside-title">{populates.name}</h2>
+
+        <p className="aside-text">const...</p>
+        <div className="heading-comments">
+          <p>
+            <MdRecommend /> {populates.count_likes}
+          </p>
+          <p>
+            <BsChat /> {populates.count_saves}
+          </p>
+          <p>
+            <IoBookmark /> {populates.count_comments}
+          </p>
+        </div>
+        <button className="btn btn-primary">
+          <p className="aside-view-yarn">See full loop</p>
+        </button>
       </div>
     </>
   );
--- frontend/src/components/Loopit/Aside/index.jsx
+++ frontend/src/components/Loopit/Aside/index.jsx
@@ -1,30 +1,46 @@
-import { BsArrowReturnRight } from "react-icons/bs";
 import "./Aside.css";
 import AsideItem from "./AsideItem";
+import { useState, useEffect } from "react";
+import loopit from "../../../api/loopit";
 
 const Aside = ({ oC }) => {
-  const populates = [
-    {
-      id: 0,
-      title: "Fizz Buzz",
-      content: "The best way to learn to code is to code.",
-    },
-    {
-      id: 1,
-      title: "Palindrome",
-      content: "the most common interview question.",
-    },
-    {
-      id: 2,
-      title: "Crud",
-      content: "the base of all web applications.",
-    },
-    {
-      id: 3,
-      title: "Hello world in cobol",
-      content: "the old way of saying hello world.",
-    },
-  ];
+  const [populates, setPopulates] = useState([]);
+
+  const fetchPopulates = async () => {
+    try {
+      const response = await loopit.get("/loops/moreLiked");
+      console.log(response.data.loops);
+      setPopulates(response.data.loops);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchPopulates();
+  }, []);
+  // const populates = [
+  //   {
+  //     id: 0,
+  //     title: "Fizz Buzz",
+  //     content: "The best way to learn to code is to code.",
+  //   },
+  //   {
+  //     id: 1,
+  //     title: "Palindrome",
+  //     content: "the most common interview question.",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Crud",
+  //     content: "the base of all web applications.",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Hello world in cobol",
+  //     content: "the old way of saying hello world.",
+  //   },
+  // ];
   return (
     <aside className={"aside " + oC}>
       <div className="aside-container">
