b88916fe02e1fd995698e7dbf5e0693f1f9f614d
Nachop51 <nachoperalta0@gmail.com>
2023-05-16 22:50:33 -0300

    Adding dockerfile, docker compose, and nginx conf for frontend

--- frontend/src/api/loopit.js
+++ frontend/src/api/loopit.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const loopit = axios.create({
-  withCredentials: true,
-  // baseURL: "http://localhost:3000/api",
-  // ! Use localhost for development
-  baseURL: "https://loopit-mvp.com/api/",
+	withCredentials: true,
+	baseURL: "http://localhost:3000/api",
+	// ! Use localhost for development
+	// baseURL: "https://loopit-mvp.com/api/",
 });
 
 export default loopit;
